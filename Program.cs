944bdfce43817d891e1eb0f171a1635029a638ec
yoh1496 <yoh1496@gmail.com>
2020-10-09 11:44:31 +0900

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnetcore_opencvsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
